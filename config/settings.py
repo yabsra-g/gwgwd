e93fce3078d4561319a5bd7202962f5f60fc1b6d
Yabsra Gebretsadik <51964599+yabsra-g@users.noreply.github.com>
2026-03-19 15:51:18 +0400

    Make migrations

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,9 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
